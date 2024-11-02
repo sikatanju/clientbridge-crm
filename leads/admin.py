e7221c9ce88b35e0ec9277371a0b924bda6687ea
sikatanju <saadtanju@gmail.com>
2024-11-02 13:02:14 +0600

    Lead list and details added.

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import User, Agent, Lead
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Lead
 
 # Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    # return HttpResponse('Hello World')
+    return render(request, 'leads/lead_list.html', { 'leads': leads})
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(pk=pk)
+
+    return render(request, 'leads/lead_detail.html', { 'lead' : lead })
