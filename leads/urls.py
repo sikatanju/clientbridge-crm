02065b7887d3b53649386e47af4e478942b4b0ee
sikatanju <saadtanju@gmail.com>
2024-11-03 10:57:32 +0600

    Implemented CRUD operation for Lead

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import lead_list, lead_detail
+from . import views
 
 urlpatterns = [
-    path('', lead_list),
-    path('<pk>/', lead_detail)
+    path('', views.lead_list, name='lead-list'),
+    path('<int:pk>/', views.lead_detail , name='lead-detail'),
+    path('<int:pk>/update', views.lead_update, name='lead-update'),
+    path('<int:pk>/delete', views.lead_delete, name='lead-delete'),
+    path('create/', views.lead_create, name='lead-create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 def lead_list(request):
@@ -14,3 +15,75 @@ def lead_detail(request, pk):
     lead = Lead.objects.get(pk=pk)
 
     return render(request, 'leads/lead_detail.html', { 'lead' : lead })
+
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+
+
+    return render(request, "leads/lead_create.html", {'form': form})
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    form = LeadModelForm(instance=lead)
+    
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            lead.save()
+            return redirect('/leads')
+        
+    return render(request, 'leads/lead_update.html', { 'lead' : lead, 'form': form})
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    lead.delete()
+    return redirect('/leads')
+
+"""
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect('/leads')
+
+
+    return render(request, "leads/lead_create.html", {'form': form})
+
+
+
+
+def lead_update(request, pk):
+    form = LeadForm()
+    lead = Lead.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            lead.first_name = form.cleaned_data['first_name']
+            lead.last_name = form.cleaned_data['last_name']
+            lead.age = form.cleaned_data['age']
+            lead.agent = Agent.objects.first()
+            lead.save()
+            return redirect('/leads')
+    return render(request, 'leads/lead_update.html', { 'lead' : lead, 'form': form})
+
+"""
